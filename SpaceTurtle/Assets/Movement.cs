117e0643c0ef7b45ccff57378ec242c0c45ba2ca
Cameron burridge <cmburridge4022@gmail.com>
2020-05-06 14:35:16 -0600

    Added idle animated character with movement

---
+++ SpaceTurtle/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public float upSpeed;
+	public float speed;
+	public Transform characterPos;
+	
+	void Start () 
+	{
+		
+	}
+	
+	void FixedUpdate () 
+	{
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(characterPos.up * speed * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(characterPos.up * upSpeed * Time.deltaTime);
+		}
+	}
+}
