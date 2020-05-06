2b074ed6e5cfbe58472b84523e0de7aa2612906e
Cameron burridge <cmburridge4022@gmail.com>
2020-05-06 16:36:05 -0600

    Added Health bar to canvas and randomly spawning moving enemies

---
+++ SpaceTurtle/Assets/EnemyMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+	public float speed;
+	public Transform enemyPos;
+	
+
+	void Update () {
+		transform.Translate(enemyPos.right * speed * Time.deltaTime);
+	}
+}
---
+++ SpaceTurtle/Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class Spawner : MonoBehaviour
+{
+    
+    public Transform spawnLocal;
+    public GameObject[] objects;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRandom());
+    }
+
+    IEnumerator SpawnRandom()
+    {
+        yield return new WaitForSeconds(Random.Range(3, 7));
+        Instantiate(objects[UnityEngine.Random.Range(0, objects.Length - 1)], spawnLocal.position, spawnLocal.rotation);
+        StartCoroutine(SpawnRandom());
+    }
+
+}
