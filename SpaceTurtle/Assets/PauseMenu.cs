8e7c35092e59c5d725cc3924f6a54d1946b05cba
Cameron burridge <cmburridge4022@gmail.com>
2020-05-06 21:31:43 -0600

    Added Main menu and Pause menu

---
+++ SpaceTurtle/Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+	public static bool gameIsPaused = false;
+	public GameObject pauseMenuUi;
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (gameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
+
+	public void Resume()
+	{
+		pauseMenuUi.SetActive(false);
+		Time.timeScale = 1f;
+		gameIsPaused = false;
+	}
+
+	void Pause()
+	{
+		pauseMenuUi.SetActive(true);
+		Time.timeScale = 0f;
+		gameIsPaused = true;
+	}
+
+	public void LoadMenu()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
+}
---
+++ SpaceTurtle/Assets/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManager : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+		Debug.Log("Quit");
+	}
+
+}
